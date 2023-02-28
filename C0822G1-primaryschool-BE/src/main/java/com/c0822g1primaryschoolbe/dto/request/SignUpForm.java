11ddd9a734c8a9cc8866a8267b6a1243c3cf2763
TranVanSy2005 <tranvansy197@gmail.com>
2023-02-28 15:55:16 +0700

    SyTV add forder for JUnit5

---
+++ C0822G1-primaryschool-BE/src/main/java/com/c0822g1primaryschoolbe/dto/request/SignUpForm.java
@@ -0,0 +1,2 @@
+package com.c0822g1primaryschoolbe.dto.request;public class SignUpForm {
+}
---
+++ C0822G1-primaryschool-BE/src/test/java/com/c0822g1primaryschoolbe/controller/SecurityController_login.java
@@ -0,0 +1,144 @@
+package com.c0822g1primaryschoolbe.controller;
+
+import com.c0822g1primaryschoolbe.dto.request.SignInForm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityController_login {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void login_username_13() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                content(this.objectMapper.writeValueAsString(signInForm))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_username_14() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("");
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                content(this.objectMapper.writeValueAsString(signInForm))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_username_15() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("áhdkjashkdjsahkj");
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_username_16() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("a");
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_username_17() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_password_13() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("sytv");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_password_14() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("sytv");
+        signInForm.setPassword("");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_password_15() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("sytv");
+        signInForm.setPassword("%^^$&$&^");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+
+    @Test
+    public void login_signInForm_18() throws Exception {
+        SignInForm signInForm = new SignInForm();
+        signInForm.setUsername("sytv");
+        signInForm.setPassword("456");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/sign-in").
+                        content(this.objectMapper.writeValueAsString(signInForm))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+        ;
+    }
+
+}
